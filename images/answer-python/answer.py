95bb140f59783cc1346cf376905c4c58de336c01
Anton Haugen <antonhaugen@mac.com>
2024-08-29 15:40:31 -0400

    results added

--- images/answer-python/answer.py
+++ images/answer-python/answer.py
@@ -8,7 +8,6 @@ import sqlalchemy as sa
 engine = sa.create_engine("mysql://codetest:swordfish@database/codetest")
 connection = engine.connect()
 
-tables = {'people':'people.csv', 'places': 'places.csv'}
 metadata = sa.schema.MetaData(engine)
 
 # make an ORM object to refer to the table
@@ -20,16 +19,19 @@ with open('/data/people.csv') as csv_file:
   reader = csv.reader(csv_file)
   next(reader)
   for row in reader: 
-    connection.execute(People.insert().values(name = row[0]))
+    connection.execute(People.insert().values(
+      given_name = row[0], family_name = row[1], date_of_birth = row[2], place_of_birth = row[3]))
+
 
 with open('/data/places.csv') as csv_file:
   reader = csv.reader(csv_file)
   next(reader)
   for row in reader: 
-    connection.execute(Places.insert().values(name = row[0]))
+    connection.execute(Places.insert().values(
+      city = row[0], county = row[1], country = row[2]))
 
 # output the table to a JSON file
-    
+# reference query for sql alchemy   
 query = """
 select country, count(distinct(concat(given_name,family_name,date_of_birth)))
 from people
@@ -41,11 +43,10 @@ order by 1
 
 with open('/data/summary_output.json', 'w') as json_file:
   j = sa.join(Places, People,
-         People.c.place_of_birth== Places.c.cities)
+         People.c.place_of_birth== Places.c.city)
   
   concat = sa.func.concat(People.c.given_name,People.c.family_name,People.c.date_of_birth)
   stmt = sa.select(Places.c.country,sa.func.count(concat)).select_from(j).group_by(Places.c.country)
-  
   rows = connection.execute(stmt).fetchall()
-  rows = [{row[0]: row[1]} for row in rows]
+  rows = {row[0]: row[1] for row in rows}
   json.dump(rows, json_file, separators=(',', ':'))
