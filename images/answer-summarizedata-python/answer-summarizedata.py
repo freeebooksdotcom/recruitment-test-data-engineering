9d83fcbe61d37f6f6b6c2013923c6e9fa7587ef3
Anton Haugen <antonhaugen@mac.com>
2024-08-29 19:14:17 -0400

    added comments to explain functions in summarize

--- images/answer-summarizedata-python/answer-summarizedata.py
+++ images/answer-summarizedata-python/answer-summarizedata.py
@@ -13,8 +13,8 @@ metadata = sa.schema.MetaData(engine)
 People = sa.schema.Table('people', metadata, autoload=True, autoload_with=engine)
 Places = sa.schema.Table('places', metadata, autoload=True, autoload_with=engine)
 
-# reference query for functions below, (unused)
-ref_query = """
+# reference query for functions below, (unused str)
+"""
 select country, 
 count(distinct(concat(given_name,family_name,date_of_birth)))
 from people
@@ -26,19 +26,25 @@ order by 2 desc
 
 # output the table to a JSON file
 with open('/data/summary_output.json', 'w') as json_file:
+
+# inner join of query
   j = sa.join(Places, People,
          People.c.place_of_birth== Places.c.city)
 
-# concat w/ birthdate in case same named persons  
+# concat full name w/ birthdate in case of same named persons  
   concat = sa.func.concat(People.c.given_name,
                           People.c.family_name,
                           People.c.date_of_birth)
   
-# distinct in case of duplicate entries
+# count distinct col in case of duplicate entries
   count_col = sa.func.count(concat.distinct())
+
+# query and execution
   stmt = sa.select(Places.c.country,count_col)\
     .select_from(j).group_by(Places.c.country)\
     .order_by(count_col.desc())
   rows = connection.execute(stmt).fetchall()
+
+# JSON output
   rows = {row[0]: row[1] for row in rows}
   json.dump(rows, json_file, separators=(',', ':'))
